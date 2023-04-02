7bc5dbdcce779c710d78baa91a7fe4287cb1379a
Jakob <mail@bauer-jakob.de>
2023-04-02 16:57:32 +0200

    Updated bottom nav bar

--- lib/config/theme/theme_config.dart
+++ lib/config/theme/theme_config.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 enum AppTheme { Dark, Light }
 
 final themeConfigs = {
-  // AppTheme.Light: ThemeData.light(useMaterial3: true),
-  AppTheme.Light: ThemeData(
-    textTheme: const TextTheme(
-      titleMedium: TextStyle(
-        color: Colors.black,
-        fontSize: 35,
-        fontWeight: FontWeight.w600,
-      ),
-    ),
-  ),
-  AppTheme.Dark: ThemeData.dark(useMaterial3: true)
+  AppTheme.Light: ThemeData.light(useMaterial3: false),
+  // AppTheme.Light: ThemeData(
+  //   textTheme: const TextTheme(
+  //     titleMedium: TextStyle(
+  //       color: Colors.black,
+  //       fontSize: 35,
+  //       fontWeight: FontWeight.w600,
+  //     ),
+  //   ),
+  // ),
+  AppTheme.Dark: ThemeData.dark(useMaterial3: false)
 };
--- lib/widgets/mercury_bottom_navigation_bar.dart
+++ lib/widgets/mercury_bottom_navigation_bar.dart
@@ -1,53 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:glassmorphism_ui/glassmorphism_ui.dart';
-import 'package:mercury_guidance_computer/config/routes/export.dart';
-import 'package:mercury_guidance_computer/models/app_definition.dart';
+ import 'package:flutter_svg/svg.dart';
+ import 'package:mercury_guidance_computer/config/routes/export.dart';
+ import 'package:mercury_guidance_computer/models/app_definition.dart';
 
-import '../startup.dart';
+ import '../startup.dart';
 
-class MercuryBottomNaviagationBar extends StatelessWidget {
-  const MercuryBottomNaviagationBar({super.key});
+ class MercuryBottomNaviagationBar extends StatelessWidget {
 
-  static List<AppDefinition> apps = [
-    AppDefinition("Home", "/", "assets/icons/home.svg"),
-    AppDefinition("Spotify", "/spotify", "assets/icons/spotify.svg"),
-    AppDefinition("Settings", "/settings", "assets/icons/settings.svg"),
-  ];
+   const MercuryBottomNaviagationBar({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.grey[300],
-      height: 80,
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      child: ListView.builder(
-        shrinkWrap: true,
-        scrollDirection: Axis.horizontal,
-        itemCount: apps.length,
-        itemBuilder: (context, index) {
-          var app = apps[index];
-          return Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 5,
-            ),
-            child: Material(
-              color: Colors.transparent,
-              child: IconButton(
-                icon: SvgPicture.asset(app.iconPath),
-                iconSize: 60,
-                splashColor: Colors.grey[500],
-                splashRadius: 35,
-                onPressed: () {
-                  var routingService = getIt.get<RoutingService>();
-                  routingService.navigateTo(app.route);
-                },
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
+   static List<AppDefinition> apps = [
+     AppDefinition("Home", "/", "assets/icons/home.svg"),
+     AppDefinition("Spotify", "/spotify", "assets/icons/spotify.svg"),
+     AppDefinition("Settings", "/settings", "assets/icons/settings.svg"),
+   ];
+
+   @override
+   Widget build(BuildContext context) {
+     return Container(
+       alignment: Alignment.center,
+       height: 70,
+       color: Colors.grey[300],
+       child: ListView.builder(
+         shrinkWrap: true,
+         scrollDirection: Axis.horizontal,
+         itemCount: apps.length,
+         itemBuilder: (context, index) {
+           var app = apps[index];
+
+           return Material(
+             color: Colors.transparent,
+             child: IconButton(
+               icon: SvgPicture.asset(app.iconPath),
+               iconSize: 55,
+               splashColor: Colors.grey[500],
+               splashRadius: 32,
+               onPressed: () {
+                 var routingService = getIt.get<RoutingService>();
+                 routingService.navigateTo(app.route);
+               },
+             ),
+           );
+         },
+       ),
+     );
+   }
+ }
